70008cd5aa540847c8f99825eb4d81cfd20edb4a
yoshinari1209 <ky.colabolatory@gmail.com>
2025-12-25 11:25:38 +0900

    black

--- embodied/__init__.py
+++ embodied/__init__.py
@@ -1,10 +1,11 @@
-__version__ = '2.0.0'
+__version__ = "2.0.0"
 
 try:
-  import colored_traceback
-  colored_traceback.add_hook(colors='terminal')
+    import colored_traceback
+
+    colored_traceback.add_hook(colors="terminal")
 except ImportError:
-  pass
+    pass
 
 from .core import *
 
